0f0ee3f371a342203338b92ddf56c99f427cdd7f
its julii <nanji828@gmail.com>
2022-12-17 12:35:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTabsOpened = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numTabsOpened; i++)
+            {
+                string tabsOpened = Console.ReadLine();
+
+                switch (tabsOpened)
+                {
+                    case "Facebook":
+                        salary -= 150; break;
+                    case "Instagram":
+                        salary -=100; break;
+                    case "Reddit":
+                        salary -= 50; break;
+                }
+
+                if (salary <= 0)
+                {
+                    break;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+            
+        }
+    }
+}
